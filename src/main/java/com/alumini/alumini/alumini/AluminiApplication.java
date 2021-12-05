75dfb5aa41820a29ee7baba94e1c7ae25207e7bd
Siddesh Gavhane <siddeshgavhane@Siddeshs-MacBook-Pro.local>
2021-12-05 17:30:50 +0530

    first commit

---
+++ src/main/java/com/alumini/alumini/alumini/AluminiApplication.java
@@ -0,0 +1,13 @@
+package com.alumini.alumini.alumini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alumini/alumini/alumini/AluminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumini.alumini.alumini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
